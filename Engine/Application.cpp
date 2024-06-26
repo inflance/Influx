ffd05ef3855eb6432014e94fb6de34d16cb9682c
inflance <1527591692@qq.com>
2024-06-26 21:10:13 +0800

    add hash

---
+++ Engine/Application.cpp
@@ -0,0 +1,8 @@
+//
+// Created by liyun on 2024/6/25.
+//
+
+#include "Application.h"
+
+namespace influx {
+} // influx
---
+++ Engine/Application.h
@@ -0,0 +1,18 @@
+//
+// Created by liyun on 2024/6/25.
+//
+
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+#include "Engine/Core/Export.h"
+
+namespace influx {
+
+    class INFLUX_API Application
+    {
+
+    };
+} // influx
+
+#endif //APPLICATION_H
